f99b8870a74aff0510ca1a5ca0b1ce0f4215178e
Sofia Leon <sofialeon@google.com>
2021-02-22 21:59:57 -0800

    fix: naming on repo

--- samples/quickstart.js
+++ samples/quickstart.js
@@ -12,30 +12,36 @@
 //
 
 'use strict';
-
-async function main() {
+const {authenticate} = require('@google-cloud/local-auth');
+const keyfilePath =
+  '/Users/sofialeon/Downloads/client_secret_204535198706-a44qeclp37upo4oedm3p46ksckt6k587.apps.googleusercontent.com (1).json';
+async function main(principal, fullResourceName, permission) {
   // [START nodejs_policy_troubleshooter_quickstart]
   // Imports the Google Cloud client library
 
-  // remove this line after package is released
   // eslint-disable-next-line node/no-missing-require
   const {IamCheckerClient} = require('@google-cloud/policy-troubleshooter');
 
   // TODO(developer): replace with your prefered project ID.
   // const projectId = 'my-project'
 
-  // Creates a client
-  // eslint-disable-next-line no-unused-vars
-  const client = new {IamCheckerClient}();
+  const options = {
+    keyfilePath,
+    scopes: ['https://www.googleapis.com/auth/cloud-platform'],
+  };
 
-  //TODO(library generator): write the actual function you will be testing
+  // Creates a client
+  const client = new IamCheckerClient(authenticate(options));
+
   async function doSomething() {
-    console.log(
-      'Developer! Change this code so that it shows how to use the library! See comments below on structure.'
-    );
-    // const [thing] = await client.methodName({
-    // });
-    // console.info(thing);
+    const policy = await client.troubleshootIamPolicy({
+      accessTuple: {
+        principal,
+        fullResourceName,
+        permission,
+      },
+    });
+    console.log(policy);
   }
   doSomething();
   // [END nodejs_policy_troubleshooter_quickstart]
